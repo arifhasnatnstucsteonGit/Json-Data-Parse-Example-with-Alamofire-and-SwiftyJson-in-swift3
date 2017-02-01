210aa1a754f9df988115956bfbc678b4b6520488
arifhasnat.hc4u <arifhasnathc4u@gmail.com>
2017-02-02 03:38:09 +0600

    json data parsed with alamofire and swifty json

--- WebApiPractices/ViewController.swift
+++ WebApiPractices/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        getBlogJsonData()
+        
+        
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+    
+    
+    func getBlogJsonData(){
+    
+    Alamofire.request("http://teledaktar.com/api/v1/blog").responseJSON { response in
+    print(response.request)  // original URL request
+    print(response.response) // HTTP URL response
+    print(response.data)     // server data
+    print(response.result)   // result of response serialization
+        
+        
+    let swiftyJsonVar = JSON(response.result.value!)
+        
+        if let resData = swiftyJsonVar["blogs"].arrayObject {
+            
+            var arrRes = [[String:AnyObject]]() //Array of dictionary
+            
+            arrRes = resData as! [[String:AnyObject]]
+            
+            for eachdata in arrRes {
+                
+                let title = eachdata["title"]
+                let description = eachdata["description"]
+                let category = eachdata["categories"]
+                let link = eachdata["link"]
+                
+                print(title)
+                print(description)
+                print(category)
+                print(link)
+
+                
+            
+            }
+            
+            
+            
+            
+            
+            
+
+        }
+        
+       
+    
+    
+        
+    }
+
+    
+    
     }
 
 
 }
+
 
